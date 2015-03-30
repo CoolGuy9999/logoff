7841a75870532fcbfe69445d80a66a5dfdbe3ba4
cattata1056 <cattata1056@gmail.com>
2015-03-30 14:59:28 -0600

    Create logoff.bat
    and added stuff

---
+++ logoff.bat
@@ -0,0 +1,12 @@
+echo off
+color a
+echo are you leaving so soon?
+choice /m "Y for logoff, N for don't"
+goto answer%ERRORLEVEL%
+:answer1
+echo goodbye
+pause
+shutdown -l
+:answer2
+echo thanks for staying
+pause
